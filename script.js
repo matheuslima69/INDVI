f58edd037b1252e7e80d564d3ec64a63470e6377
matheuslima69 <131802037+matheuslima69@users.noreply.github.com>
2023-10-10 21:05:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector("h1").textContent= "Programação e Sistemas de Informação";
+
+document.querySelector("footer").innerHTML += <a href="https://github.com/matheuslima69/10102023.git"></a>;
